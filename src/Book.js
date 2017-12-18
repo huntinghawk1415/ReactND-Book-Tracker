6c4815027209af45c02e4b496841f56b30de496c
Jacob Lockett <jacobwyattlockett@gmail.com>
2017-12-18 05:43:20 +0000

    fixed tabbed formatting.. again, blame ocd

--- src/Book.js
+++ src/Book.js
@@ -5,28 +5,28 @@ class Book extends Component {
     return (
       <ol className="books-grid">
         { this.props.books.map(s => (
-        <li key={ s.title }>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + s.imageLinks.thumbnail + ')' }}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option value="none" disabled>Move to...</option>
-                  <option value="currentlyReading">Currently Reading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="none">None</option>
-                </select>
+          <li key={ s.title }>
+            <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + s.imageLinks.thumbnail + ')' }}></div>
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="none" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
               </div>
+              <div className="book-title">{ s.title }</div>
+              <div className="book-authors">{
+                s.authors.length === 1 ? s.authors :
+                s.authors.length > 1 ? s.authors[0] + ", and " + s.authors[1] :
+                s.authors.length > 2 ? s.authors[0] + ", " + s.authors[1] + ", and " + (s.authors.length - 2) + " other(s)" :
+                null
+              }</div>
             </div>
-            <div className="book-title">{ s.title }</div>
-            <div className="book-authors">{
-              s.authors.length === 1 ? s.authors :
-              s.authors.length > 1 ? s.authors[0] + ", and " + s.authors[1] :
-              s.authors.length > 2 ? s.authors[0] + ", " + s.authors[1] + ", and " + (s.authors.length - 2) + " other(s)" :
-              null
-            }</div>
-          </div>
           </li>
         ))}
       </ol>
