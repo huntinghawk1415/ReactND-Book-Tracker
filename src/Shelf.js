30054d0510c7e0950bee29f7ef4f8f191786f43c
Jacob <jacobwyattlockett@gmail.com>
2018-01-14 19:57:12 -0500

    view 'CHANGES.md to view detailed commit changes

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react'
-import Book from './Book'
+import React, {Component} from 'react';
+import Book from './Book';
+
+//Renders a shelf with the books that match criteria in './Book'
 
 class Shelf extends Component {
   render() {
-    return (
+    const {books, shelf, title, handleChange} = this.props;
+    return(
       <div className="bookshelf">
-        <h2 className="bookshelf-title">{ this.props.title }</h2>
+        <h2 className="bookshelf-title">{title}</h2>
         <div className="bookshelf-books">
-          <Book books={ this.props.books } />
+          <Book
+            books={books}
+            handleChange={handleChange}
+            shelf={shelf}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Shelf
+export default Shelf;
