feb323738a4f0166887edeb48554e50349cd76df
Jacob <jacobwyattlockett@gmail.com>
2018-01-14 19:53:11 -0500

    view 'CHANGES.md to view detailed commit changes

--- src/Book.js
+++ src/Book.js
@@ -1,37 +1,80 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import Select from './Select';
+import {Link} from 'react-router-dom';
+
+//Renders a 'book' based on shelf criteria
 
 class Book extends Component {
   render() {
-    return (
-      <ol className="books-grid">
-        { this.props.books.map(s => (
-          <li key={ s.title }>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + s.imageLinks.thumbnail + ')' }}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
+    const {books, shelf, handleChange} = this.props;
+    //Returns a 'no match' JSX value if no books match shelf criteria
+    if(books.reduce((r, s) => {
+      if(s.shelf === shelf) {
+        return r + 1;
+      } else {
+        return r;
+      }
+    }, 0) === 0) {
+      return (
+        <div>
+          <h4 className="mt-5">
+            You don't have anything in this bookshelf
+          </h4>
+          <Link
+            to="/search"
+            title="Add a book"
+          >
+            <button className="btn btn-sm btn-success text-white">
+              Add a book
+            </button>
+          </Link>
+        </div>
+      );
+    } else {
+      //Returns an ordered list of books that match the shelf criteria
+      return (
+        <ol className="books-grid">
+          {books.map((s, i) => (
+            s.shelf === shelf ?
+              <li key={i}>
+                <div className="book">
+                  <div className="book-top">
+                    <div
+                      className="book-cover"
+                      style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: s.hasOwnProperty('imageLinks')
+                        ? `url(${s.imageLinks.thumbnail})`
+                        : `url(${"http://www.pigsandsheep.org/wp-content/plugins/wp-ulike/assets/img/no-thumbnail.png"})`
+                      }}
+                    ></div>
+                    <Select
+                      handleChange={handleChange}
+                      index={i}
+                      selected={s.shelf}
+                    />
+                  </div>
+                  <div
+                    className="book-title text-truncate"
+                    title={s.title}
+                  >{s.title}</div>
+                  <div className="book-authors">{
+                    !s.hasOwnProperty('authors') ? "No authors available" :
+                    s.authors.length === 1 ? s.authors :
+                    s.authors.length === 2 ? `${s.authors[0]}, and ${s.authors[1]}` :
+                    `${s.authors[0]}, ${s.authors[1]}, and ${s.authors.length - 2 > 1 ?
+                      `${s.authors.length - 2} others` : `${s.authors.length -2} other`
+                    }`
+                  }</div>
                 </div>
-              </div>
-              <div className="book-title">{ s.title }</div>
-              <div className="book-authors">{
-                s.authors.length === 1 ? s.authors :
-                s.authors.length > 1 ? s.authors[0] + ", and " + s.authors[1] :
-                s.authors.length > 2 ? s.authors[0] + ", " + s.authors[1] + ", and " + (s.authors.length - 2) + " other(s)" :
-                null
-              }</div>
-            </div>
-          </li>
-        ))}
-      </ol>
-    )
+              </li>
+            : null
+          ))}
+        </ol>
+      );
+    }
   }
 }
 
-export default Book
+export default Book;
