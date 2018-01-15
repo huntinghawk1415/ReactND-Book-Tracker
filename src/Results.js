7615b0abacd90ba417cfb9b7193ffc7658d91e37
Jacob <jacobwyattlockett@gmail.com>
2018-01-14 19:55:11 -0500

    view 'CHANGES.md to view detailed commit changes

---
+++ src/Results.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import List from './List';
+
+//Renders the results or lackthereof from the user's inputs
+
+class Results extends Component {
+  render() {
+    const {query, handleChange} = this.props;
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title mt-3 mb-4">{
+          !query
+          ? `Sorry, "${this.props.input}" isn't yeilding results. Check back later.`
+          : `Results for "${this.props.input}"`
+        }</h2>
+        <div className="bookshelf-books">{
+          query &&
+            <List
+              query={query}
+              handleChange={handleChange}
+            />
+        }</div>
+      </div>
+    );
+  }
+}
+
+export default Results;
